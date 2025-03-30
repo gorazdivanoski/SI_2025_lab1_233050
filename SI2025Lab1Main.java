505d1f1f3967d4f6cf13b9215f3db6fee0d0ced9
gorazdivanoski <gorazdivanoski9@gmail.com>
2025-03-30 21:34:17 +0200

    Implement getCompletedTasks() method

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -70,7 +70,14 @@ class TaskManager {
     // 2. Find all completed tasks
     public List<Task> getCompletedTasks() {
         // TODO: Implement logic to return completed tasks
-        return new ArrayList<>();
+        // return new ArrayList<>();
+	List<Task> completed = new ArrayList<>();
+    	for (Task task : tasks) {
+            if (task.isCompleted()) {
+        	    completed.add(task);
+            }
+    	}
+    	return completed;
     }
 
     // 3. List tasks sorted by name
